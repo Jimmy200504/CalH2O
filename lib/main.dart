e81d07c5786744a92a193d85cd47c3c08973d761
Jen <jimmyjen123@gmail.com>
2025-06-07 15:53:27 +0800

    Split photo flow into two prompt

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'pages/main_page.dart';
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
@@ -12,11 +11,6 @@ void main() async {
   
   final prefs = await SharedPreferences.getInstance();
   final hasProfile = prefs.containsKey('name');
-
-  FirebaseFunctions.instanceFor(
-    region: 'us-central1',
-  ).useFunctionsEmulator('127.0.0.1', 5001);
-  debugPrint('🔧 Connected to Firebase Functions Emulator');
 
   runApp(MyApp(startFromMainPage: hasProfile));
 }
