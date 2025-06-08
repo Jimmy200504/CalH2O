1f4fe26d83c895b4f78c8a09b61b089ac37336ea
chenchu114891 <kenisthebest0907@gmail.com>
2025-06-08 20:16:01 +0800

    font and barrage

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -177,6 +177,7 @@ class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: ListView(
           padding: const EdgeInsets.all(16),
